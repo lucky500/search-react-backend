b206f5a4b8625937db5b9a8f217f73af52887ca5
Luciano Oliveira <luciano101@msn.com>
2018-02-23 23:55:10 -0500

    changes to package.json

--- src/config/server.js
+++ src/config/server.js
@@ -11,8 +11,8 @@ server.use(bodyParser.json());
 server.use(allowCors);
 server.use(queryParser());
 
-server.listen(port, function(){
-  console.log(`Backend executing on Port ${port}.`);
+server.listen(process.env.PORT || port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
 })
 
 module.exports = server;
